cd7b1c806562e435c4441567abde428d15dfcb15
Ivan <ivanglzr01@gmail.com>
2024-10-17 22:34:38 +0200

    feat: import the jwt module into the auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -9,6 +10,11 @@ import { User, UserSchema } from 'src/user/schemas/user.schema';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_KEY,
+      signOptions: { expiresIn: '1h' },
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
