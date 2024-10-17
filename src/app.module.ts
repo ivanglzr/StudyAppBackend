cef87290f0e98a0c9a5ff5169968edaa71d5b2cf
Ivan <ivanglzr01@gmail.com>
2024-10-17 19:09:12 +0200

    feat: user module created and imported into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { UserModule } from './user/user.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
+    UserModule,
   ],
 })
 export class AppModule {}
