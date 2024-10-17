8ee927acd170c73d51f9bbb7f41ce96543a66b97
Ivan <ivanglzr01@gmail.com>
2024-10-17 22:24:12 +0200

    feat: register endpoint created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 
-@Controller('auth')
-export class AuthController {}
+import { AuthService } from './auth.service';
+
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+
+import { AUTH_ROUTES } from 'src/common/routes';
+
+@Controller(AUTH_ROUTES.BASE)
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post(AUTH_ROUTES.REGISTER)
+  async register(@Body() user: CreateUserDto) {
+    await this.authService.postUser(user);
+
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'User created',
+    };
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
 import type { Model } from 'mongoose';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { User } from 'src/user/schemas/user.schema';
+
+import { hashPassword } from 'src/common/functions/hash-password.function';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
+  async emailExists(email: string): Promise<boolean> {
+    return !!(await this.userModel.findOne({ email }));
+  }
+
+  async postUser(user: CreateUserDto) {
+    const emailExists = await this.emailExists(user.email);
+
+    if (emailExists) throw new ConflictException('Email already exists');
+
+    const hashedPassword = hashPassword(user.password);
+
+    const newUser = new this.userModel({
+      ...user,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+  }
 }
