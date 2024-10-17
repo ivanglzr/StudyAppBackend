d503fb01748b23b571021258953531eedba7736f
Ivan <ivanglzr01@gmail.com>
2024-10-17 22:00:55 +0200

    fix: import functions instead of bcryptjs (i don't know why this solved the problem)

--- src/common/functions/hash-password.function.ts
+++ src/common/functions/hash-password.function.ts
@@ -1,4 +1,4 @@
-import bcryptjs from 'bcryptjs';
+import { genSaltSync, hashSync } from 'bcryptjs';
 
 import { SALT_ROUNDS } from '../config';
 
@@ -8,9 +8,9 @@ import { SALT_ROUNDS } from '../config';
  * @returns The hashed password
  */
 export function hashPassword(password: string): string {
-  const salt = bcryptjs.genSaltSync(SALT_ROUNDS);
+  const salt = genSaltSync(SALT_ROUNDS);
 
-  const hassedPassword = bcryptjs.hashSync(password, salt);
+  const hassedPassword = hashSync(password, salt);
 
   return hassedPassword;
 }
