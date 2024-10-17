16ce043ccb2eaeaf9f7be1d3ed7e415ed2d0cbf9
Ivan <ivanglzr01@gmail.com>
2024-10-17 20:21:42 +0200

    chore: variables for user validation

--- src/user/config.ts
+++ src/user/config.ts
@@ -1,3 +1,8 @@
+export const fullnameMinLength = 2;
+
 export const emailRegex = /^\S+@\S+\.\S+$/;
+export const emailMinLength = 5;
+
 export const passwordRegex =
   /^(?=(.*[A-Z]){2,})(?=(.*[a-z]){2,})(?=(.*\d){1,})(?=(.*[!@#$%^&*()_\-+=[\]{};':"\\|,.<>/?]){1,}).{8,}$/;
+export const passwordMinLength = 8;
