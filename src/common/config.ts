e247a659cbe209e6381c2eb6073748dfe5dc6cc0
Ivan <ivanglzr01@gmail.com>
2024-10-18 19:21:40 +0200

    chore: create an enum for enviroment variables names

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,3 +1,9 @@
 export const SALT_ROUNDS = 10;
 
 export const accessTokenName = 'access_token';
+
+export enum ENVIROMENT_VARIABLES {
+  PORT = 'PORT',
+  MONGODB_URI = 'MONGODB_URI',
+  JWT_KEY = 'JWT_KEY',
+}
