f62f045f09685c6fe62a6f8f18e693a4ad37ff93
Ivan <ivanglzr01@gmail.com>
2024-10-19 12:48:41 +0200

    chore: get user function created in user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
 import { Model } from 'mongoose';
@@ -8,4 +8,12 @@ import { User } from './schemas/user.schema';
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async getUser(id: string): Promise<User> {
+    const user = await this.userModel.findById(id);
+
+    if (!user) throw new NotFoundException('User not found');
+
+    return user;
+  }
 }
