b6bac63c0686b4416468a3412d3cc091e94cd47a
Ivan <ivanglzr01@gmail.com>
2024-10-19 12:48:17 +0200

    feat: import the jwt module into the user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
+
+import { JwtModule } from '@nestjs/jwt';
+
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 import { User, UserSchema } from './schemas/user.schema';
 
-import { UserController } from './user.controller';
+import { ENVIROMENT_VARIABLES } from 'src/common/config';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>(ENVIROMENT_VARIABLES.JWT_KEY),
+      }),
+    }),
   ],
   providers: [UserService],
   controllers: [UserController],
