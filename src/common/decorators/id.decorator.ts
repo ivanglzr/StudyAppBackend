fd31d61cfb40aeecdbcc77fa54d7d0769a835c71
Ivan <ivanglzr01@gmail.com>
2024-10-19 12:46:46 +0200

    chore: create a decorator to extract the id from the request

--- src/common/decorators/id.decorator.ts
+++ src/common/decorators/id.decorator.ts
@@ -1,9 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const Id = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
-    const req = ctx.switchToHttp().getRequest();
+export const Id = createParamDecorator((_, ctx: ExecutionContext) => {
+  const req = ctx.switchToHttp().getRequest();
 
-    return data ? req.session[data] : req.session.id;
-  },
-);
+  return req.user.id;
+});
