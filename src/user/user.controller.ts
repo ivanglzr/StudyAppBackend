fdca6af890567e1ce4f48a83b23d3cdd5d527821
Ivan <ivanglzr01@gmail.com>
2024-10-19 12:49:23 +0200

    feat: auth guard and get user implemented

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
+
+import { UserService } from './user.service';
+
+import { AuthGuard } from 'src/common/guards/auth.guard';
+
+import { Id } from 'src/common/decorators/id.decorator';
 
 import { USER_ROUTES } from 'src/common/routes';
 
 @Controller(USER_ROUTES.BASE)
-export class UserController {}
+@UseGuards(AuthGuard)
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getUser(@Id() id: string) {
+    const user = await this.userService.getUser(id);
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'User found',
+      user,
+    };
+  }
+}
