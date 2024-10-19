8f37a32ed4f60b3f83744f566ae90070852197de
Ivan <ivanglzr01@gmail.com>
2024-10-19 21:50:53 +0200

    chore: create the change password method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
 import { Model } from 'mongoose';
+
+import { hashPassword } from 'src/common/functions/hash-password.function';
+import { validatePassword } from 'src/common/functions/validate-password.function';
 
 import { User } from 'src/common/schemas/user.schema';
 
@@ -17,5 +24,19 @@ export class UserService {
     if (!user) throw new NotFoundException('User not found');
 
     return user;
+  }
+
+  async changePassword(id: string, password: string, newPassword: string) {
+    const user = await this.userModel.findById(id);
+
+    if (!user) throw new NotFoundException('User not found');
+
+    const isPasswordValid = validatePassword(password, user.password);
+
+    if (!isPasswordValid) throw new UnauthorizedException('Unauthorized');
+
+    user.password = hashPassword(newPassword);
+
+    await user.save();
   }
 }
