a024bcdffcef810a50b92558958e4788bcfaca08
Ivan <ivanglzr01@gmail.com>
2024-10-21 16:37:02 +0200

    chore: create error messages for HTTP errors

---
+++ src/common/messages.ts
@@ -0,0 +1,7 @@
+export enum ERROR_MESSAGES {
+  EMAIL_IN_USE = 'Email is already taken, please use another one',
+  USER_NOT_FOUND = "User doesn't exist, please register",
+  LOG_IN_UNAUTHORIZED = 'Log in unauthorized, please try again',
+  ID_NOT_VALID = "The id isn't valid, please use a correct one",
+  PASSWORD_NOT_VALID = 'Password is incorrect, please try again',
+}
