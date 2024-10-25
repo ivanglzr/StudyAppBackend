cdcf946d46d66f954663e706c51837c29049b0de
Ivan <ivanglzr01@gmail.com>
2024-10-25 19:13:25 +0200

    chore: import subject schema in subject module

--- src/subject/subject.module.ts
+++ src/subject/subject.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { SubjectController } from './subject.controller';
 import { SubjectService } from './subject.service';
 
+import {
+  Subject,
+  SubjectSchema,
+} from 'src/common/schemas/subject/subject.schema';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Subject.name, schema: SubjectSchema }]),
+  ],
   controllers: [SubjectController],
   providers: [SubjectService],
 })
