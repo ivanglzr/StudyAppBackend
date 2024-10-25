1e284ac767132dfb613b22790bff2066aa8d77f8
Ivan <ivanglzr01@gmail.com>
2024-10-25 23:18:52 +0200

    feat: get subjects and post subject

--- src/subject/subject.controller.ts
+++ src/subject/subject.controller.ts
@@ -1,6 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  UseGuards,
+  Body,
+  Post,
+} from '@nestjs/common';
 
-import { SUBJECT_ROUTES } from 'src/common/routes';
+import { SubjectService } from './subject.service';
 
-@Controller(SUBJECT_ROUTES.BASE)
-export class SubjectController {}
+import { AuthGuard } from 'src/user/guards/auth.guard';
+
+import { Id } from 'src/user/decorators/id.decorator';
+
+import { CreateSubjectDto } from './dto/create-subject.dto';
+
+@Controller()
+@UseGuards(AuthGuard)
+export class SubjectController {
+  constructor(private readonly subjectService: SubjectService) {}
+
+  @Get()
+  async getSubjects(@Id() userId: string) {
+    const subjects = await this.subjectService.findAll(userId);
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Subjects found',
+      subjects,
+    };
+  }
+
+  @Post()
+  async postSubject(@Id() userId: string, @Body() subject: CreateSubjectDto) {
+    this.subjectService.createSubject(userId, subject);
+
+    return {
+      statusCode: 201,
+      message: 'Subject created',
+    };
+  }
+}
--- src/subject/subject.service.ts
+++ src/subject/subject.service.ts
@@ -1,13 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { Subject } from 'src/common/schemas/subject/subject.schema';
 
+import { CreateSubjectDto } from './dto/create-subject.dto';
+
 @Injectable()
 export class SubjectService {
   constructor(
     @InjectModel(Subject.name) private readonly subjectModel: Model<Subject>,
   ) {}
+
+  async subjectExists(subjectName: string): Promise<boolean> {
+    return !!(await this.subjectModel.findOne({ subjectName }));
+  }
+
+  async findAll(userId: string) {
+    return this.subjectModel.find({ userId });
+  }
+
+  async createSubject(userId: string, subject: CreateSubjectDto) {
+    if (this.subjectExists(subject.subjectName))
+      throw new ConflictException(
+        'Subject already exists, please use another name',
+      );
+
+    const newSubject = new this.subjectModel({
+      userId,
+      ...subject,
+    });
+
+    await newSubject.save();
+  }
 }
