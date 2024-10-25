e330a0fa2466fa0f92fcca91492cdb3f190f0dd1
Ivan <ivanglzr01@gmail.com>
2024-10-25 20:21:46 +0200

    chore: inject subject model in subject service

--- src/subject/subject.service.ts
+++ src/subject/subject.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Subject } from 'src/common/schemas/subject/subject.schema';
+
 @Injectable()
-export class SubjectService {}
+export class SubjectService {
+  constructor(
+    @InjectModel(Subject.name) private readonly subjectModel: Model<Subject>,
+  ) {}
+}
