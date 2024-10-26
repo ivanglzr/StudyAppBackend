ab3ba7f835af530c2720e187559eab1dfd31ae8f
Ivan <ivanglzr01@gmail.com>
2024-10-26 16:12:57 +0200

    fix: search the subject by its id

--- src/subject/note/note.controller.ts
+++ src/subject/note/note.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, UseGuards } from '@nestjs/common';
+
 import { AuthGuard } from 'src/common/guards/auth.guard';
 
-import { NOTE_ROUTES } from 'src/common/routes';
-import { Id } from 'src/user/decorators/id.decorator';
 import { NoteService } from './note.service';
+
+import { Id } from 'src/user/decorators/id.decorator';
+
+import { ValidateIdPipe } from 'src/common/pipes/validate-id.pipe';
+
+import { NOTE_ROUTES } from 'src/common/routes';
+
+import { subjectIdParamName } from './config';
 
 @Controller(NOTE_ROUTES.BASE)
 @UseGuards(AuthGuard)
@@ -11,8 +18,11 @@ export class NoteController {
   constructor(private readonly noteService: NoteService) {}
 
   @Get()
-  async getNotes(@Id() userId: string) {
-    const notes = await this.noteService.getNotes(userId);
+  async getNotes(
+    @Param(subjectIdParamName, ValidateIdPipe) subjectId: string,
+    @Id() userId: string,
+  ) {
+    const notes = await this.noteService.getNotes(userId, subjectId);
 
     const message =
       notes.length === 0 ? "There isn't any notes" : 'Notes found';
--- src/subject/note/note.service.ts
+++ src/subject/note/note.service.ts
@@ -12,12 +12,12 @@ export class NoteService {
     @InjectModel(Subject.name) private readonly subjectModel: Model<Subject>,
   ) {}
 
-  async getNotes(userId: string) {
+  async getNotes(userId: string, subjectId: string) {
     const subject = await this.subjectModel
-      .findOne({ userId })
+      .findOne({ userId, _id: subjectId })
       .select(['notes'] as Array<keyof Subject>);
 
-    if (!subject) throw new NotFoundException('Notes not found');
+    if (!subject) throw new NotFoundException('Subject not found');
 
     return subject.notes;
   }
