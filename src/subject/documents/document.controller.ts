51866a4660bd67782776546e3a2b5e087bb326a1
Ivan <ivanglzr01@gmail.com>
2024-11-01 23:37:59 +0100

    feat: post document

--- src/subject/documents/document.controller.ts
+++ src/subject/documents/document.controller.ts
@@ -1,6 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpStatus,
+  Param,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { DocumentService } from './document.service';
+
+import { AuthGuard } from 'src/common/guards/auth.guard';
+
+import { Id } from 'src/common/decorators/id.decorator';
+
+import { ValidateIdPipe } from 'src/common/pipes/validate-id.pipe';
+
+import { multerStorage } from './config';
+
+import { subjectIdParamName } from '../config';
 
 import { DOCUMENT_ROUTES } from 'src/common/routes';
 
 @Controller(DOCUMENT_ROUTES.BASE)
-export class DocumentController {}
+@UseGuards(AuthGuard)
+export class DocumentController {
+  constructor(private readonly documentService: DocumentService) {}
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file0', {
+      storage: multerStorage,
+    }),
+  )
+  async uploadDocument(
+    @Id() userId: string,
+    @Param(subjectIdParamName, ValidateIdPipe) subjectId: string,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    await this.documentService.postDocument(userId, subjectId, file.filename);
+
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'Document uploaded',
+    };
+  }
+}
--- src/subject/documents/document.service.ts
+++ src/subject/documents/document.service.ts
@@ -1,4 +1,34 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+
 import { Injectable } from '@nestjs/common';
 
+import { SubjectService } from '../subject.service';
+
+import { documentsDestination } from './config';
+
 @Injectable()
-export class DocumentService {}
+export class DocumentService {
+  constructor(private readonly subjectService: SubjectService) {}
+
+  async deleteFile(filename: string) {
+    try {
+      const filePath = path.join(process.cwd(), documentsDestination, filename);
+
+      console.log(filePath);
+
+      await fs.unlink(filePath);
+    } catch (e) {}
+  }
+
+  async postDocument(userId: string, subjectId: string, filename: string) {
+    const subject = await this.subjectService.findSubjectById(
+      userId,
+      subjectId,
+    );
+
+    subject.documents.push(filename);
+
+    await subject.save();
+  }
+}
