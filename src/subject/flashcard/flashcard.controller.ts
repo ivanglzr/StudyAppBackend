da867125d6cc3c3d678de60d190e2696dacee519
Ivan <ivanglzr01@gmail.com>
2024-11-02 17:16:34 +0100

    feat: get flashcards

--- src/subject/flashcard/flashcard.controller.ts
+++ src/subject/flashcard/flashcard.controller.ts
@@ -1,6 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, UseGuards } from '@nestjs/common';
+
+import { FlashcardService } from './flashcard.service';
+
+import { AuthGuard } from 'src/common/guards/auth.guard';
+
+import { Id } from 'src/common/decorators/id.decorator';
+
+import { ValidateIdPipe } from 'src/common/pipes/validate-id.pipe';
+
+import { subjectIdParamName } from '../config';
 
 import { FLASHCARD_ROUTES } from 'src/common/routes';
 
 @Controller(FLASHCARD_ROUTES.BASE)
-export class FlashcardController {}
+@UseGuards(AuthGuard)
+export class FlashcardController {
+  constructor(private readonly flashcardService: FlashcardService) {}
+
+  @Get()
+  async getFlashcards(
+    @Id() userId: string,
+    @Param(subjectIdParamName, ValidateIdPipe) subjectId: string,
+  ) {
+    const flashcards = await this.flashcardService.getFlashcards(
+      userId,
+      subjectId,
+    );
+
+    const message =
+      flashcards.length === 0
+        ? "There isn't any flashcards"
+        : 'Flashcards found';
+
+    return {
+      statusCode: HttpStatus.OK,
+      message,
+      flashcards,
+    };
+  }
+}
--- src/subject/flashcard/flashcard.service.ts
+++ src/subject/flashcard/flashcard.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+import { SubjectService } from 'src/subject/subject.service';
+
 @Injectable()
-export class FlashcardService {}
+export class FlashcardService {
+  constructor(private readonly subjectService: SubjectService) {}
+
+  async getFlashcards(userId: string, subjectId: string) {
+    const { flashcards } = await this.subjectService.findSubjectById(
+      userId,
+      subjectId,
+    );
+
+    return flashcards;
+  }
+}
