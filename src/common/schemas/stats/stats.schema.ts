6aac3bebc5d92730317e515c36f30cbef6bb12af
Ivan <ivanglzr01@gmail.com>
2024-11-23 17:00:47 +0100

    chore: stats schema

---
+++ src/common/schemas/stats/stats.schema.ts
@@ -0,0 +1,43 @@
+import * as mongoose from 'mongoose';
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+
+import { User } from '../user/user.schema';
+import {
+  SubjectStats,
+  SubjectStatsSchema,
+} from './subjectStats/subjectStats.schema';
+import {
+  LearnedFlashcards,
+  LearnedFlashcardsSchema,
+} from './learnedFlashcards/learnedFlashcards.schema';
+
+@Schema()
+export class Stats {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+  userId: User;
+
+  @Prop({ default: 0 })
+  totalTime: number;
+
+  @Prop({ type: [SubjectStatsSchema], default: [] })
+  subjectStats: SubjectStats[];
+
+  @Prop({
+    type: LearnedFlashcardsSchema,
+    default: { percentage: 0, total: 0, subjects: [] },
+  })
+  learnedFlashcards: LearnedFlashcards;
+}
+
+export const StatsSchema = SchemaFactory.createForClass(Stats);
+
+StatsSchema.pre('save', async function (next) {
+  if (!Array.isArray(this.subjectStats)) return next();
+
+  this.totalTime = this.subjectStats.reduce(
+    (acc, curr) => acc + curr.studyTime,
+    0,
+  );
+
+  next();
+});
