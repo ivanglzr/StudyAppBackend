c6a25cfd57877c46cffef4441008f78e596a6d87
Ivan <ivanglzr01@gmail.com>
2024-11-23 17:00:19 +0100

    chore: subject stats schema

---
+++ src/common/schemas/stats/subjectStats/subjectStats.schema.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose';
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+
+import { Subject } from 'src/common/schemas/subject/subject.schema';
+
+@Schema()
+export class SubjectStats {
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Subject',
+    required: true,
+  })
+  subjectId: Subject;
+
+  @Prop({ required: true })
+  studyTime: number;
+}
+
+export const SubjectStatsSchema = SchemaFactory.createForClass(SubjectStats);
