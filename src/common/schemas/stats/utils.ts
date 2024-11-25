8ed2720ef1e3591460c34f9dc7953ff952ed2349
Ivan <ivanglzr01@gmail.com>
2024-11-25 22:15:13 +0100

    chore: changed utils functions

--- src/common/schemas/stats/utils.ts
+++ src/common/schemas/stats/utils.ts
@@ -1,51 +1,20 @@
 import { Subject } from '../subject/subject.schema';
-import { LearnedFlashcards } from './learnedFlashcards/learnedFlashcards.schema';
 
-export const countTotalFlashcards = (subjects: Subject[]) =>
-  subjects.reduce((acc, curr) => acc + curr.flashcards.length, 0);
+import { LearnedFlashcardsPerSubject } from './learnedFlashcards/learnedFlashcards.schema';
 
-export const countLearnedFlashcards = (subjects: Subject[]) =>
-  subjects.reduce(
-    (acc, curr) =>
-      acc +
-      curr.flashcards.reduce((acc, curr) => acc + (curr.learned ? 1 : 0), 0),
+export const getSubjectFlashcardStats = (
+  subject: Subject,
+): LearnedFlashcardsPerSubject => {
+  const total = subject.flashcards.length;
+  const learned = subject.flashcards.reduce(
+    (acc, curr) => acc + (curr.learned ? 1 : 0),
     0,
   );
 
-export const countLearnedFlashcardsPerSubject = (subjects: Subject[]) =>
-  subjects.map((subject) => {
-    const total = subject.flashcards.length;
-    const learned = subject.flashcards.reduce(
-      (acc, curr) => acc + (curr.learned ? 1 : 0),
-      0,
-    );
-
-    return {
-      learnedFlashcardsPercentage: total !== 0 ? learned / total : 0,
-      totalFlashcards: total,
-      learnedFlashcards: learned,
-      subjectId: subject._id,
-    };
-  });
-
-export function getFlashcardStats(subjects: Subject[]) {
-  const learnedFlashcardsPerSubject =
-    countLearnedFlashcardsPerSubject(subjects);
-
-  const totalFlashcards = countTotalFlashcards(subjects);
-
-  const learnedFlashcards = countLearnedFlashcards(subjects);
-
-  const flashcardsStats = new LearnedFlashcards();
-
-  flashcardsStats.totalFlashcards = totalFlashcards;
-
-  flashcardsStats.learnedFlashcardsPercentage =
-    totalFlashcards !== 0 ? learnedFlashcards / totalFlashcards : 0;
-
-  flashcardsStats.subjectsFlashcardsStats = learnedFlashcardsPerSubject;
-
-  flashcardsStats.learnedFlashcards = learnedFlashcards;
-
-  return flashcardsStats;
-}
+  return {
+    learnedFlashcardsPercentage: total !== 0 ? learned / total : 0,
+    totalFlashcards: total,
+    learnedFlashcards: learned,
+    subjectId: subject._id,
+  };
+};
